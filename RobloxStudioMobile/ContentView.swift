4de2a6893a0b409b5dec151a468a2315cfce5325
Andy <alin21@stuy.edu>
2021-03-28 18:02:16 -0400

    mocked up home page

--- RobloxStudioMobile/ContentView.swift
+++ RobloxStudioMobile/ContentView.swift
@@ -9,8 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                NavigationLink(destination: ExploreView()){
+                    Label("Explore", systemImage: "house")
+                }
+                NavigationLink(destination: FileView()){
+                    Label("Files", systemImage: "folder")
+                }
+                NavigationLink(
+                    destination: SettingView(),
+                    label: {
+                        Label("Settings", systemImage: "gear")
+                    })
+                NavigationLink(
+                    destination: EditingView()
+                        .navigationBarHidden(true),
+                    label: {
+                        Text("Dev")
+                    })
+                Section(header: Text("Favorites")){
+                    
+                }
+            }.listStyle(SidebarListStyle())
+            .navigationTitle("Home")
+            .toolbar{
+                EditButton()
+            }
+            ExploreView()
+        }
+    }
+}
+
+struct ExploreView: View{
+    var body: some View{
+        Text("Explore placeholder")
+            .navigationTitle("Explore")
+    }
+}
+
+struct FileView: View{
+    var body: some View{
+        Text("Files placeholder")
+            .navigationTitle("Files")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "plus")
+                    })
+                }
+            }
+    }
+}
+
+struct SettingView: View{
+    var body: some View{
+        Text("Settings placeholder")
+            .navigationTitle("Settings")
     }
 }
 
--- RobloxStudioMobile/RobloxStudioMobileApp.swift
+++ RobloxStudioMobile/RobloxStudioMobileApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RobloxStudioMobileApp: App {
     var body: some Scene {
         WindowGroup {
-            EditingView()
+            ContentView()
         }
     }
 }
