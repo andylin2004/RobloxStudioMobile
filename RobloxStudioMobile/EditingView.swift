f864880d7f75ab89bea8dc3cef5162b34245def1
Andy <alin21@stuy.edu>
2021-03-28 17:29:25 -0400

    top button layout complete

---
+++ RobloxStudioMobile/EditingView.swift
@@ -0,0 +1,66 @@
+//
+//  EditingView.swift
+//  RobloxStudioMobile
+//
+//  Created by Andy Lin on 3/28/21.
+//
+
+import SwiftUI
+
+struct EditingView: View {
+
+    var body: some View {
+        NavigationView{
+            GeometryReader{ geometry in
+                HStack{
+                    VStack{
+                        ListView()
+                    }.frame(width: geometry.size.width*0.25)
+                    Divider()
+                    VStack{
+                        Text("Enviromental Placeholder")
+                    }.frame(minWidth: 0, maxWidth: .infinity)
+                    Divider()
+                    VStack{
+                        Text("Properties Placeholder")
+                    }.frame(width: geometry.size.width*0.25)
+                    
+                }
+                
+            }.navigationBarTitle("meme", displayMode: .inline)
+            .toolbar{
+                ToolbarItemGroup(placement: .navigationBarLeading){
+                    Button(action:{}, label:{
+                        Text("Done")
+                    }).buttonStyle(PlainButtonStyle())
+                    Button(action: {}, label: {
+                        Image(systemName: "sidebar.left")
+                    })
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.uturn.backward.circle")
+                    })
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.uturn.forward.circle")
+                    })
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing){
+                    Button(action: {}, label: {
+                        Image(systemName: "square.and.arrow.up")
+                    })
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "info.circle")
+                    })
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "sidebar.right")
+                    })
+                }
+            }
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct EditingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditingView()
+    }
+}
