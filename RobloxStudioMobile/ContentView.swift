9adce6ed0f2ff076ba550a60e3b7a72985b8db32
Andy <alin21@stuy.edu>
2021-03-29 15:25:44 -0400

    moving out code not driving UI to main program

--- RobloxStudioMobile/ContentView.swift
+++ RobloxStudioMobile/ContentView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import UniformTypeIdentifiers
 
 struct ContentView: View {
     var body: some View {
@@ -107,43 +106,10 @@ struct FileView: View{
     }
 }
 
-struct InputDocument: FileDocument {
-    
-    static var readableContentTypes: [UTType] { [.xml] }
-    
-    var input: String
-    
-    init(input: String) {
-        self.input = input
-    }
-    
-    init(configuration: FileDocumentReadConfiguration) throws {
-        guard let data = configuration.file.regularFileContents,
-              let string = String(data: data, encoding: .utf8)
-        else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-        input = string
-    }
-    
-    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        return FileWrapper(regularFileWithContents: input.data(using: .utf8)!)
-    }
-    
-}
-
 struct SettingView: View{
     var body: some View{
         Text("Settings placeholder")
             .navigationTitle("Settings")
-    }
-}
-
-extension UTType {
-    // Word documents are not an existing property on UTType
-    static var rbxlx: UTType {
-        // Look up the type from the file extension
-        UTType.types(tag: "rbxlx", tagClass: .filenameExtension, conformingTo: .xml).first!
     }
 }
 
--- RobloxStudioMobile/RobloxStudioMobileApp.swift
+++ RobloxStudioMobile/RobloxStudioMobileApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 @main
 struct RobloxStudioMobileApp: App {
@@ -13,5 +14,38 @@ struct RobloxStudioMobileApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+struct InputDocument: FileDocument {
+    
+    static var readableContentTypes: [UTType] { [.xml] }
+    
+    var input: String
+    
+    init(input: String) {
+        self.input = input
+    }
+    
+    init(configuration: FileDocumentReadConfiguration) throws {
+        guard let data = configuration.file.regularFileContents,
+              let string = String(data: data, encoding: .utf8)
+        else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+        input = string
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        return FileWrapper(regularFileWithContents: input.data(using: .utf8)!)
+    }
+    
+}
+
+extension UTType {
+    // Word documents are not an existing property on UTType
+    static var rbxlx: UTType {
+        // Look up the type from the file extension
+        UTType.types(tag: "rbxlx", tagClass: .filenameExtension, conformingTo: .xml).first!
     }
 }
