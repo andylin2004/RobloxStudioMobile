fe09b72992a1f59e0e12cd73d8677061b27a3aee
Andy <alin21@stuy.edu>
2021-03-28 22:06:27 -0400

    upload test removed, uploading now works

--- RobloxStudioMobile/ContentView.swift
+++ RobloxStudioMobile/ContentView.swift
@@ -29,11 +29,6 @@ struct ContentView: View {
                         label: {
                             Text("Dev")
                         })
-                    NavigationLink(
-                        destination: UploadTest(),
-                        label: {
-                            Text("Upload")
-                        })
                 }
                 Section(header: Text("Favorites")){
                     
@@ -56,23 +51,78 @@ struct ExploreView: View{
 }
 
 struct FileView: View{
+    @State var showDocumentViewer = false
+    @State var fileContent = ""
     var body: some View{
         Text("Files placeholder")
             .navigationTitle("Files")
             .toolbar{
                 ToolbarItem(placement: .navigationBarTrailing){
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Menu{
+                        Button(
+                            action: {EditingView()},
+                            label: {
+                                Label("Add New", systemImage: "doc.badge.plus")
+                            }
+                        )
+                        Button(
+                            action: {showDocumentViewer = true},
+                            label: {Label("Import from Files", systemImage: "square.and.arrow.down")
+                            }
+                        )
+                    }
+                    label: {
                         Image(systemName: "plus")
-                    })
+                    }
                 }
             }
+            .sheet(isPresented: self.$showDocumentViewer, content: {
+                DocumentPicker(fileContent: $fileContent)
+            })
     }
 }
+
 
 struct SettingView: View{
     var body: some View{
         Text("Settings placeholder")
             .navigationTitle("Settings")
+    }
+}
+
+struct DocumentPicker: UIViewControllerRepresentable{
+    @Binding var fileContent: String
+    
+    func makeCoordinator() -> DocumentPickerCoordinator {
+        return DocumentPickerCoordinator(fileContent: $fileContent)
+    }
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
+        let controller: UIDocumentPickerViewController
+        
+        controller = UIDocumentPickerViewController(forOpeningContentTypes: [.data], asCopy: true)
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {}
+}
+
+class DocumentPickerCoordinator: NSObject, UIDocumentPickerDelegate, UINavigationControllerDelegate{
+    
+    @Binding var fileContent: String
+    
+    init(fileContent: Binding<String>){
+        _fileContent = fileContent
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]){
+        let fileURL = urls[0]
+        do{
+            fileContent = try String(contentsOf: fileURL, encoding: .utf8)
+            print(fileContent)
+        }catch let error{
+            print(error.localizedDescription)
+        }
     }
 }
 
